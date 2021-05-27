08e44227f05b59645226a312355465fd24d2efcd
willy <willysmarc@gmail.com>
2021-05-26 21:16:50 -0300

    Primeiro Commit

---
+++ android/app/src/main/kotlin/com/example/projeto_quiz_engsoftlab/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.projeto_quiz_engsoftlab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/helloworld.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Teste extends StatefulWidget {
+  @override
+  _TesteState createState() => _TesteState();
+}
+
+class _TesteState extends State<Teste> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello World"),
+      ),
+    );
+  }
+}
